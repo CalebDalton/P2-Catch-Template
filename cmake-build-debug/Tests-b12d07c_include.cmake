49bd94a93b81bdb27b704055b2a30b565d2ae465
CalebDalton <calebsmdalton@gmail.com>
2025-07-02 13:02:45 -0400

    Initial push

---
+++ cmake-build-debug/Tests-b12d07c_include.cmake
@@ -0,0 +1,5 @@
+if(EXISTS "E:/School/COP3530/P2-Catch-Template/cmake-build-debug/Tests-b12d07c_tests.cmake")
+  include("E:/School/COP3530/P2-Catch-Template/cmake-build-debug/Tests-b12d07c_tests.cmake")
+else()
+  add_test(Tests_NOT_BUILT-b12d07c Tests_NOT_BUILT-b12d07c)
+endif()
